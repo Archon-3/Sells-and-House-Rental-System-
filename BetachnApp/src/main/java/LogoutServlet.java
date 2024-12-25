9118984dc9e6e72fbd723ae7c69c1c9d0d75aa7a
hp <hp@DESKTOP-1Q82HD6>
2024-12-25 09:05:35 -0800

    second commit

---
+++ BetachnApp/src/main/java/LogoutServlet.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/LogoutServlet")
+public class LogoutServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Invalidate the current session
+        HttpSession session = request.getSession(false); // Fetch the current session, do not create a new one
+        if (session != null) {
+            session.invalidate(); // End the session
+        }
+
+        // Redirect to the login page
+        response.sendRedirect("LoginTo.html");
+    }
+}
